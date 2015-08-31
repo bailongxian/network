14d077dba8f28a1fc251929d32e52ee8383732cc
bailongxian <bljswpu@163.com>
2015-08-31 11:16:25 +0800

    task info

---
+++ Mysql/Mysql_task.cpp
@@ -0,0 +1,11 @@
+#include "Mysql_task.h
+
+CMysql_Task::CMysql_Task()
+{
+
+}
+
+CMysql_Task::~Mysql_Task()
+{
+
+}
---
+++ Mysql/Mysql_task.h
@@ -0,0 +1,12 @@
+#ifndef _MYSQL_TASK_H_
+#define _MYSQL_TASK_H_
+
+class CMysql_Task
+{
+public:
+	CMysql_Task();
+	~CMysql_Task();
+private:
+};
+
+#endif
